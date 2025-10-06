532e14310fac8ebdb21b73fe5eb4c4e48f6c5dfa
Enzo Bettini <enzo.bettini@coopertec.com.br>
2025-10-06 13:43:59 -0300

    Finishing Lessons 06/10/2025

---
+++ Backend/Section 24/3.2 Express Server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello!</h1>")
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>how are you!</h1>")
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>contact me!</h1>")
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
